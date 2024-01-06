638411178686fa7a2e92cc7bffd1ebf8c04bee28
Jörg Singer <singer.joerg@gmx.de>
2024-01-06 21:55:41 +0100

    Add active state to ScopeNavigation component

--- src/components/ScopeNavigation/ScopeNavigation.tsx
+++ src/components/ScopeNavigation/ScopeNavigation.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import classes from "./ScopeNavigation.module.css";
 import { useNavigate } from "react-router-dom";
 import { SymbolTableContext } from "../App/App";
@@ -10,13 +10,15 @@ export interface ScopeNavigationProps {}
 function ScopeNavigation(_props: ScopeNavigationProps) {
   const symbolTable = useContext(SymbolTableContext);
   const navigate = useNavigate();
+  const [active, setActive] = useState(0);
+
   return (
     <Stack
-      className={classes.SymbolNavigation}
+      className={classes.ScopeNavigation}
       data-testid="ScopeNavigation"
       gap="xs"
     >
-      {symbolTable.scopes.map((scope) => {
+      {symbolTable.scopes.map((scope, index) => {
         let parent: number | null = scope.parent;
         let parentName = getParentName(parent, symbolTable, false);
         let scopeName = getScopeName(scope, symbolTable, false);
@@ -25,8 +27,13 @@ function ScopeNavigation(_props: ScopeNavigationProps) {
           <NavLink
             className={classes.NavLink}
             key={scope.my_id}
-            onClick={() => navigate(`/scopes/${scope.my_id}`)}
+            active={index === active}
+            onClick={() => {
+              setActive(index);
+              navigate(`/scopes/${scope.my_id}`);
+            }}
             label={`${scope.my_id} - ${scopeName} parent: (${parentName})`}
+            variant="filled"
           ></NavLink>
         );
       })}
